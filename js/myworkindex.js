444e09bf63cacc9a36c6f4bac1eed97bb1efdad3
Matthew Stevens <mrzeapple42@gmail.com>
2020-03-01 22:33:48 -0600

    fixed cover

--- js/myworkindex.js
+++ js/myworkindex.js
@@ -2,9 +2,10 @@ var mainimg1 = "Yes";
 
 function change1(){
     document.getElementById('largework').style.background = img1;
-    document.getElementById('largework').style.backgroundSize = "contain";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
     mainimg = img1;
     mainimg1 = "Yes";
     largeworklink = "https://www.matthewstevens.me/3d-design/";
@@ -13,9 +14,10 @@ function change1(){
 }
 function change2(){
     document.getElementById('largework').style.background = img2;
-    document.getElementById('largework').style.backgroundSize = "100%";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
     mainimg = img2;
     mainimg1 = "No";
     largeworklink = "https://www.matthewstevens.me/web-design/";
@@ -24,9 +26,10 @@ function change2(){
 }
 function change3(){
     document.getElementById('largework').style.background = img3;
-    document.getElementById('largework').style.backgroundSize = "100%";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
     mainimg = img3;
     mainimg1 = "No";
     largeworklink = "https://www.matthewstevens.me/photography/";
@@ -35,37 +38,42 @@ function change3(){
 }
 function change1hover(){
     document.getElementById('largework').style.background = img1;
-    document.getElementById('largework').style.backgroundSize = "contain";
-    document.getElementById('largework').style.backgroundPosition = "center";
-    document.getElementById('largework').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largeworktitle').innerText = "3D Design";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework').style.width = "90%";
 }
 function change2hover(){
     document.getElementById('largework').style.background = img2;
-    document.getElementById('largework').style.backgroundSize = "100%";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
     document.getElementById('largeworktitle').innerText = "Web Design";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
 }
 function change3hover(){
     document.getElementById('largework').style.background = img3;
-    document.getElementById('largework').style.backgroundSize = "100%";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
     document.getElementById('largeworktitle').innerText = "Photography";
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
 }
 function changeout(){
     if(mainimg1 == "Yes"){
     document.getElementById('largework').style.background = mainimg;
-    document.getElementById('largework').style.backgroundSize = "contain";
-    document.getElementById('largework').style.backgroundPosition = "center";
+    document.getElementById('largework').style.backgroundSize = "cover";
     document.getElementById('largework').style.backgroundRepeat = "no-repeat";
     document.getElementById('largeworktitle').innerText = maintext;
+    document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
     }else{
         document.getElementById('largework').style.background = mainimg;
-        document.getElementById('largework').style.backgroundSize = "100%";
-        document.getElementById('largework').style.backgroundPosition = "center";
+        document.getElementById('largework').style.backgroundSize = "cover";
         document.getElementById('largework').style.backgroundRepeat = "no-repeat";
         document.getElementById('largeworktitle').innerText = maintext;
+        document.getElementById('largework').style.borderRadius = "36px";
+    document.getElementById('largework').style.width = "90%";
     }
 }
