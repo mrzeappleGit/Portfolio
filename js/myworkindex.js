33b80daf5a3ccc5d773c856a5f83a1a2e481800e
Matthew Stevens <mrzeapple42@gmail.com>
2020-03-14 16:37:29 -0500

    setup new items

--- js/myworkindex.js
+++ js/myworkindex.js
@@ -1,13 +1,14 @@
 var mainimg3d_set = "Yes";
 var mainimg_web_set = "Yes";
+var mainimg_photo_set = "Yes";
 //3D Section
 function change_3d_1(){
     document.getElementById('largework-3d').style.background = img_3d_1;
     document.getElementById('largework-3d').style.backgroundSize = "cover";
     document.getElementById('largework-3d').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-3d').style.width = "90%";
-    mainimg1 = img1;
-    mainimg1_set = "Yes";
+    mainimg_3d = img_3d_1;
+    mainimg3d_set = "Yes";
     largeworklink_3d = "https://www.matthewstevens.me/3d-design/";
     document.getElementById('largeworktitle-3d').innerText = "Neon Corner";
     maintext_3d = "Neon Corner";
@@ -17,8 +18,8 @@ function change_3d_2(){
     document.getElementById('largework-3d').style.backgroundSize = "cover";
     document.getElementById('largework-3d').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-3d').style.width = "90%";
-    mainimg1 = img2;
-    mainimg1_set = "No";
+    mainimg_3d = img_3d_2;
+    mainimg3d_set = "No";
     largeworklink_3d = "https://www.matthewstevens.me/3d-design/";
     document.getElementById('largeworktitle-3d').innerText = "Dubuque Soccer Club";
     maintext_3d = "Dubuque Soccer Club";
@@ -28,8 +29,8 @@ function change_3d_3(){
     document.getElementById('largework-3d').style.backgroundSize = "cover";
     document.getElementById('largework-3d').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-3d').style.width = "90%";
-    mainimg = img3;
-    mainimg1 = "No";
+    mainimg_3d = img_3d_3;
+    mainimg3d_set = "No";
     largeworklink_3d = "https://www.matthewstevens.me/photography/";
     document.getElementById('largeworktitle-3d').innerText = "Merge Performance";
     maintext_3d = "Merge Performance";
@@ -73,7 +74,7 @@ function changeout_3d(){
 
 //Web Design
 function change_web_1(){
-    document.getElementById('largework-web').style.background = img_2eb_1;
+    document.getElementById('largework-web').style.background = img_web_1;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-web').style.width = "90%";
@@ -84,62 +85,131 @@ function change_web_1(){
     maintext_web = "Inspiration Stables";
 }
 function change_web_2(){
-    document.getElementById('largework-web').style.background = img2;
+    document.getElementById('largework-web').style.background = img_web_2;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-web').style.width = "90%";
-    mainimg1 = img2;
-    mainimg1_set = "No";
+    mainimg_web_1 = img_web_2;
+    mainimg_web_set = "No";
     largeworklink = "https://www.matthewstevens.me/3d-design/";
     document.getElementById('largeworktitle-web').innerText = "Easier Games";
     maintext_web = "Easier Games";
 }
 function change_web_3(){
-    document.getElementById('largework-web').style.background = img3;
+    document.getElementById('largework-web').style.background = img_web_3;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-web').style.width = "90%";
-    mainimg = img3;
-    mainimg1 = "No";
+    mainimg_web = img_web_3;
+    mainimg_web_set = "No";
     largeworklink = "https://www.matthewstevens.me/photography/";
     document.getElementById('largeworktitle-web').innerText = "Merge Performance";
     maintext_web = "Merge Performance";
 }
 function change_web_1_hover(){
-    document.getElementById('largework-web').style.background = img1;
+    document.getElementById('largework-web').style.background = img_web_1;
     document.getElementById('largework-web').style.backgroundSize = "cover";
-    document.getElementById('largeworktitle-web').innerText = "Neon Corner";
+    document.getElementById('largeworktitle-web').innerText = "Inspiration Stables";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
     document.getElementById('largework-web').style.width = "90%";
 }
 function change_web_2_hover(){
-    document.getElementById('largework-web').style.background = img2;
+    document.getElementById('largework-web').style.background = img_web_2;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
-    document.getElementById('largeworktitle-web').innerText = "Dubuque Soccer Club";
+    document.getElementById('largeworktitle-web').innerText = "Easier Games";
     document.getElementById('largework-web').style.width = "90%";
 }
 function change_web_3_hover(){
-    document.getElementById('largework-web').style.background = img3;
+    document.getElementById('largework-web').style.background = img_web_3;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
-    document.getElementById('largeworktitle-web').innerText = "Merge Performance";
+    document.getElementById('largeworktitle-web').innerText = "Easier Tickets";
     document.getElementById('largework-web').style.width = "90%";
 }
 function changeout_web(){
-    if(mainimg1 == "Yes"){
-    document.getElementById('largework-web').style.background = mainimg1;
+    if(mainimg_web == "Yes"){
+    document.getElementById('largework-web').style.background = mainimg_web;
     document.getElementById('largework-web').style.backgroundSize = "cover";
     document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
-    document.getElementById('largeworktitle-web').innerText = maintext_3d;
+    document.getElementById('largeworktitle-web').innerText = maintext_web;
     document.getElementById('largework-web').style.width = "90%";
     }else{
-        document.getElementById('largework-web').style.background = mainimg1;
+        document.getElementById('largework-web').style.background = mainimg_web;
         document.getElementById('largework-web').style.backgroundSize = "cover";
         document.getElementById('largework-web').style.backgroundRepeat = "no-repeat";
-        document.getElementById('largeworktitle-web').innerText = maintext_3d;
+        document.getElementById('largeworktitle-web').innerText = maintext_web;
         document.getElementById('largework-web').style.width = "90%";
     }
 }
 
 //Photography
+function change_photo_1(){
+    document.getElementById('largework-photo').style.background = img_photo_1;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework-photo').style.width = "90%";
+    mainimg_photo = img_photo_1;
+    mainimg_photo_set = "Yes";
+    largeworklink_photo = "https://www.matthewstevens.me/photo/";
+    document.getElementById('largeworktitle-photo').innerText = "Inspiration Stables";
+    maintext_photo = "Inspiration Stables";
+}
+function change_photo_2(){
+    document.getElementById('largework-photo').style.background = img_photo_2;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework-photo').style.width = "90%";
+    mainimg_photo_1 = img_photo_2;
+    mainimg_photo_set = "No";
+    largeworklink = "https://www.matthewstevens.me/3d-design/";
+    document.getElementById('largeworktitle-photo').innerText = "Easier Games";
+    maintext_photo = "Easier Games";
+}
+function change_photo_3(){
+    document.getElementById('largework-photo').style.background = img_photo_3;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework-photo').style.width = "90%";
+    mainimg_photo = img_photo_3;
+    mainimg_photo_set = "No";
+    largeworklink = "https://www.matthewstevens.me/photography/";
+    document.getElementById('largeworktitle-photo').innerText = "Merge Performance";
+    maintext_photo = "Merge Performance";
+}
+function change_photo_1_hover(){
+    document.getElementById('largework-photo').style.background = img_photo_1;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largeworktitle-photo').innerText = "Inspiration Stables";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largework-photo').style.width = "90%";
+}
+function change_photo_2_hover(){
+    document.getElementById('largework-photo').style.background = img_photo_2;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largeworktitle-photo').innerText = "Easier Games";
+    document.getElementById('largework-photo').style.width = "90%";
+}
+function change_photo_3_hover(){
+    document.getElementById('largework-photo').style.background = img_photo_3;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largeworktitle-photo').innerText = "Easier Tickets";
+    document.getElementById('largework-photo').style.width = "90%";
+}
+function changeout_photo(){
+    if(mainimg_photo == "Yes"){
+    document.getElementById('largework-photo').style.background = mainimg_photo;
+    document.getElementById('largework-photo').style.backgroundSize = "cover";
+    document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+    document.getElementById('largeworktitle-photo').innerText = maintext_photo;
+    document.getElementById('largework-photo').style.width = "90%";
+    }else{
+        document.getElementById('largework-photo').style.background = mainimg_photo;
+        document.getElementById('largework-photo').style.backgroundSize = "cover";
+        document.getElementById('largework-photo').style.backgroundRepeat = "no-repeat";
+        document.getElementById('largeworktitle-photo').innerText = maintext_photo;
+        document.getElementById('largework-photo').style.width = "90%";
+    }
+}
